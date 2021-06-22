64e7a0441179c52ac22e59b63cfa8f1b92e19d66
Moisés <moisesadc534@gmail.com>
2021-06-22 14:09:18 -0300

    js

---
+++ main.js
@@ -0,0 +1,19 @@
+// DOM Document Onject Model
+/*Abre  e fecha o menu quando clicar no icone: hamburguer e x*/
+const nav = document.querySelector('#header nav') //procura o seletor
+const toggle = document.querySelectorAll("nav .toggle")
+
+for(const element of toggle){
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+
+/*Quando clicar em um item do menu, esconder o menu*/
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
