2db5ef05cbd4e69a3c5c3ffe1f1eb227f9065396
Moisés <moisesadc534@gmail.com>
2021-06-24 13:07:04 -0300

    Arrumando códigos

--- main.js
+++ main.js
@@ -30,3 +30,30 @@ window.addEventListener('scroll', function () {
    header.classList.remove('scroll')
   }
 })
+
+/*Testimonials-Swiper*/
+const swiper = new Swiper('.swiper-container', {
+  slidesPerview: 1,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  mousewheel: true,
+  keyboard:true,
+});
+
+/*ScrollReveal: mostrar elementos quando der rolagem na página*/
+const scrollReveal = ScrollReveal({
+  origin:'top',
+  distance:'30px',
+  duration:700,
+  reset:true
+})
+
+scrollReveal.reveal(
+`#home .image, #home .text, 
+#about .image, #about .text,
+#services header, #services .card,
+#testimonials header, #testimonials .testimonials
+#contact .text, #contact .links`,
+
+{interval: 100})
